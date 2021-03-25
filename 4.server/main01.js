9bf10bc8569b60595fac1c1354505cb2b2e745c8
pony <18780587015@163.com>
2021-03-25 11:04:45 +0800

    安装了express

--- 4.server/main01.js
+++ 4.server/main01.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 
 //探索request
 // const server = http.createServer((request, response) => {
@@ -29,25 +29,25 @@ const http = require('http');
 // });
 
 //响应json格式数据
-const server = http.createServer((request, response) => {
+// const server = http.createServer((request, response) => {
    
-    const data = {
-        id:1,
-        title:'关山月',
-        content:'明月出天山，苍茫云海间'
-    }
+//     const data = {
+//         id:1,
+//         title:'关山月',
+//         content:'明月出天山，苍茫云海间'
+//     }
 
-    const jsonData = JSON.stringify(data);
+//     const jsonData = JSON.stringify(data);
 
-    response.writeHead(200,{
-        'content-Type':'application/json;charset=utf-8'
-    });
+//     response.writeHead(200,{
+//         'content-Type':'application/json;charset=utf-8'
+//     });
 
-    response.write(jsonData);
+//     response.write(jsonData);
 
-    response.end();
-  });
+//     response.end();
+//   });
 
-server.listen(3000, () => {
-    console.log('服务已启动！！！');
-});
+// server.listen(3000, () => {
+//     console.log('服务已启动！！！');
+// });
--- src/main.js
+++ src/main.js
@@ -1,2 +1,45 @@
-const greeting = '你好';
-console. log (greeting);
+const express = require ('express');
+const app = express ();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('服务已启动！！！')
+});
+
+app.get('/',(request,response)=>{
+  response.send('你好啊');
+})
+
+const data = [
+{
+        id:1,
+        title:'关山月',
+        content:'明月出天山，苍茫云海间'
+},
+ {
+    id:2,
+    title:'望岳',
+    content:'会当临绝顶，一览众山小'
+}
+,
+{
+    id:3,
+    title:'忆江南',
+    content:'日出江花红似火，春来江水绿如蓝'
+}
+];
+
+app.get ('/post',(request,response) =>{
+    response.send(data);
+});
+
+app.get('/post/:postId', (request, response) => {
+    // 获取ID
+    const {postId } = request.params;
+
+    // 查找具体内容
+    const posts = data.filter(item => item.id == postId );
+
+    //做出响应
+    response.send(posts[0]);
+})
