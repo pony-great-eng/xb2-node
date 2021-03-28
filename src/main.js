18bfe218cd378f2fa221d0977379bd43e0d62308
pony <18780587015@163.com>
2021-03-28 11:45:02 +0800

    设置头部数据

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ const express = require ('express');
 const app = express ();
 const port = 3000;
 
+/**
+ * 使用中间件
+ */
+app.use(express.json());
+
 app.listen(port, () => {
     console.log('服务已启动！！！')
 });
@@ -42,4 +47,29 @@ app.get('/post/:postId', (request, response) => {
 
     //做出响应
     response.send(posts[0]);
+});
+
+/**
+ * 创建内容
+ */
+
+app.post('/post', (request, response) => {
+    //获取请求里的数据
+    const {content} = request.body;
+
+    //设置状态码
+
+    response.status('201');
+
+    //输出头部数据
+
+    console.log(request.headers['Sing']);
+
+    //设置响应头部数据
+    response.set('Sing-Along', 'how i wonder what you are');
+
+    //做出相应
+    response.send ({
+        message:'成功创建了内容：{content}'
+    });
 })
