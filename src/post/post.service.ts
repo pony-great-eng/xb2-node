9d63a5ab6daa6ad6ccbf71a58924aa27adbebcd3
pony <18780587015@163.com>
2021-04-01 18:11:54 +0800

    数据处理

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,5 +1,5 @@
 import {connection} from '../app/database/mysql';
-
+import {PostModel} from './post.model';
 /**
  * 获取内容列表
  */
@@ -19,5 +19,57 @@ export const getPosts = async () => {
     `;
 
     const [data] = await connection.promise().query(statement);
+    return data;
+}
+
+/**
+ * 创建内容
+ */
+export const createPost = async (post: PostModel) => {
+    //准备查询
+    const statement =`
+    INSERT INTO post
+    SET ?
+    `;
+    
+    //执行查询
+    const [data]  =await connection.promise().query(statement, post);
+
+    //提供数据
+    return data;
+}
+
+/**
+ * 更新内容
+ */
+ export const updatePost = async (postId:number, post: PostModel) => {
+    //准备查询
+    const statement =`
+    UPDATE post
+    SET ?
+    WHERE id=?
+    `;
+    
+    //执行查询
+    const [data]  =await connection.promise().query(statement, [post,postId]);
+
+    //提供数据
+    return data;
+}
+
+/**
+ * 删除内容
+ */
+ export const deletePost = async (postId:number) => {
+    //准备查询
+    const statement =`
+    DELETE FROM post
+    WHERE id=?
+    `;
+    
+    //执行查询
+    const [data]  =await connection.promise().query(statement, postId);
+
+    //提供数据
     return data;
 }
