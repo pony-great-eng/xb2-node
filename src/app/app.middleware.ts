a1d7eceafa7172070c77ef88cfa43c2e553142c8
pony <18780587015@163.com>
2021-04-03 15:44:33 +0800

    用户登录

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -44,6 +44,14 @@ export const defaultErrorHandler = (
             statusCode =409;
             message = '用户名已被占用';
             break;
+            case 'USER_DOES_NOT_EXIST':
+            statusCode =400;
+            message = '用户名不存在';
+            break;
+            case 'PASSWORD_DOES_NOT_MATCH':
+            statusCode =400;
+            message = '密码不对';
+             break;
         default:
             statusCode = 500;
             message='服务暂时出点问题~~';
